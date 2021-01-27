0bac11a3e846d6b9c9b6cb9976c41965d2a37538
Khrystyna Skvarok <christina.skvarok@gmail.com>
2021-01-27 22:30:15 +0100

    fix: fixed script execution moment

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ const fetchBooks = () => {
     })
     .then(data => {
       books = data?.books;
+
+      const bookList = document.getElementById('bookList');
+      bookList.innerHTML = '';
+
+      books.map(book => {
+        bookList.innerHTML += `<li class="book ${book.status}"><a class="book-title" href="${book.url}">${book.title}</a> by ${book.author}</li>`;
+      });
     });
 };
 
 fetchBooks().catch(err => {
   console.log("Ouch, can't find or read the file!", err);
 });
-
-window.onload = function() {
-  const bookList = document.getElementById('bookList');
-  bookList.innerHTML = '';
-  books.map(book => {
-    bookList.innerHTML += `<li class="book ${book.status}"><a class="book-title" href="${book.url}">${book.title}</a> by ${book.author}</li>`;
-  });
-};
